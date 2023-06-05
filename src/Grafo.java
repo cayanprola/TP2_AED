7de42286bd725d226b1d98c21fdc05442726ccff
999diogo <999Samurai@protonmail.com>
2023-06-05 21:42:42 +0100

    Changes

--- src/Grafo.java
+++ src/Grafo.java
@@ -1,7 +1,5 @@
 import java.text.DecimalFormat;
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.Map;
+import java.util.*;
 
 public class Grafo {
     private Map<String, Map<String, Integer>> mapa;
@@ -44,8 +42,41 @@ public class Grafo {
             double percentage = Double.parseDouble(f.format((double) pesos / numberAbove));
             percentages.putIfAbsent(percentage, new ArrayList<>());
             percentages.get(percentage).add(key);
-        };
+        }
 
-        System.out.println(percentages.toString());
+
+        TreeMap<Double, ArrayList<String>> sorted = new TreeMap<>(Collections.reverseOrder());
+        sorted.putAll(percentages);
+        List<Double> keys = new ArrayList<>(sorted.keySet());
+
+        int cont = 1;
+        percentages.clear();
+        for (Map.Entry<Double, ArrayList<String>> entry : sorted.entrySet()) {
+            for (String submissao : entry.getValue()) {
+                if (cont < 3) {
+                    percentages.putIfAbsent(entry.getKey(), new ArrayList<>());
+                    percentages.get(entry.getKey()).add(submissao);
+                } else {
+                    // .get(2) para pegar a 3 maior percentagem para fazer o ranking 3
+                    percentages.putIfAbsent(keys.get(2), new ArrayList<>());
+                    percentages.get(keys.get(2)).add(submissao);
+                }
+            }
+            cont += 1;
+        }
+
+        sorted.clear();
+        sorted.putAll(percentages);
+        cont = 1;
+
+        for (Map.Entry<Double, ArrayList<String>> entry : sorted.entrySet()) {
+            System.out.print(cont + ": ");
+            for (String submissao : entry.getValue()) {
+                System.out.print(submissao + " ");
+            }
+            System.out.print("(" + entry.getKey() + "%)");
+            System.out.println();
+            cont += 1;
+        }
     }
 }
